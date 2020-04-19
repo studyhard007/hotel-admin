cc678fca995d9832b79bd9c82fac81fda14210ec
kenny <yuanzhijiang@pinquest.cn>
2020-04-20 01:09:36 +0800

    fix: performance

--- src/Pages/Performance/index.tsx
+++ src/Pages/Performance/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Card } from "antd";
+import moment from "moment";
 import { Chart, Geom, Axis, Tooltip } from "bizcharts";
 import "./index.scss";
-
-//  type PerformanceState = {
-//    list: 
-//  }
+type PerformanceModel = {
+  price?: number;
+  billinquiryin?: string;
+};
+type PerformanceState = {
+  list: Array<PerformanceModel> | null;
+};
 class Performance extends React.Component {
+  state: PerformanceState = {
+    list: null,
+  };
   titleList = [
     {
       title: "昨日入住数",
@@ -26,39 +33,62 @@ class Performance extends React.Component {
     },
   ];
   componentDidMount() {
-  
+    try {
+      fetch("http://localhost:3000/api/v1/getperformance", {
+        method: "post",
+        headers: {
+          Accept: "application/json,text/plain, */*",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `start_at=${moment()
+          .add(-7, "day")
+          .format("YYYYMMDD")}&end_at=${moment()
+          .add(-1, "day")
+          .format("YYYYMMDD")}`,
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          this.setState({
+            list: data.data,
+          });
+        });
+    } catch (error) {
+      console.log(error);
+    }
   }
   render() {
-    const data = [
-      {
-        year: "2020-4-11",
-        checkin: 15,
-      },
-      {
-        year: "2020-4-12",
-        checkin: 33,
-      },
-      {
-        year: "2020-4-13",
-        checkin: 10,
-      },
-      {
-        year: "2020-4-14",
-        checkin: 11,
-      },
-      {
-        year: "2020-4-15",
-        checkin: 20,
-      },
-      {
-        year: "2020-4-16",
-        checkin: 33,
-      },
-      {
-        year: "2020-4-17",
-        checkin: 11,
-      },
-    ];
+    // const data = [
+    //   {
+    //     year: "2020-4-11",
+    //     checkin: 15,
+    //   },
+    //   {
+    //     year: "2020-4-12",
+    //     checkin: 33,
+    //   },
+    //   {
+    //     year: "2020-4-13",
+    //     checkin: 10,
+    //   },
+    //   {
+    //     year: "2020-4-14",
+    //     checkin: 11,
+    //   },
+    //   {
+    //     year: "2020-4-15",
+    //     checkin: 20,
+    //   },
+    //   {
+    //     year: "2020-4-16",
+    //     checkin: 33,
+    //   },
+    //   {
+    //     year: "2020-4-17",
+    //     checkin: 11,
+    //   },
+    // ];
     const cols = {
       sales: {
         tickInterval: 100,
@@ -76,16 +106,16 @@ class Performance extends React.Component {
           ))}
         </div>
         <Card title="酒店入住业绩">
-          <Chart height={450} data={data} scale={cols} forceFit>
-            <Axis name="year" />
-            <Axis name="checkin" />
+          <Chart height={450} data={this.state.list} scale={cols} forceFit>
+            <Axis name="billinquiryin" />
+            <Axis name="price" />
             <Tooltip
             // crosshairs用于设置 tooltip 的辅助线或者辅助框
             // crosshairs={{
             //  type: "y"
             // }}
             />
-            <Geom type="interval" position="year*checkin" />
+            <Geom type="interval" position="billinquiryin*price" />
           </Chart>
         </Card>
       </div>
